0b0915793ff2879a9beacc44ed01ff3872013562
M. H. TAMIM <mustaqhasnattamim13@gmail.com>
2026-04-25 20:53:41 +0600

    Add files via upload

---
+++ emergency_router.py
@@ -0,0 +1,8 @@
+# ---------------------------------------------------------------------------
+# Project: Emergency Dispatch Routing System (Rural Bangladesh)
+# Author: Mustaq Hasnat Tamim
+# Description: This tool calculates the quickest path from a GPS coordinate 
+#              to the nearest emergency facility using a custom-digitized 
+#              road network.
+# Note: Requires ArcGIS Pro with Network Analyst Extension.
+# ---------------------------------------------------------------------------
